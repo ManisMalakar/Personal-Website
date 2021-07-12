d011a8287dbf69d9d704cc1b7cc88a9c59eb344e
Manish Mali <mani.malakar12@gmail.com>
2021-07-12 10:58:37 -0400

    Final edits

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,6 +5,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/Caldwell.png',
       date: '2020-2021',
+      link:'https://www.caldwell.edu/',
       desc:
      'Lead the role for the migration of university webpages from WordPress 5.5.3 to WordPress 5.4.2 Gutenberg'
     },
@@ -13,6 +14,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/MaskDetector.jpg',
       date: '2021-01',
+      link:'https://github.com/ManisMalakar/Face-Mask-Detector',
       desc:
      'A real-time face detection script in order to detect face masks in static images as well as in real-time video streams using Transfer Learning and model MobileNetV2.'
     },
@@ -21,6 +23,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/library.jpg',
       date: '2020-12',
+      link:'https://github.com/ManisMalakar/LibraryManagementSystem',
       desc:
      'A library management system implementation using concepts of OOP in JAVA, GUI desgined using Java Swing and MS SQL as database.'
     },
@@ -29,6 +32,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/FaceDetection.png',
       date: '2020-09',
+      link:'https://github.com/ManisMalakar/RealTime-Face-Detection',
       desc:
      'A program to detect the faces of individual while using a real-time webcam.'
     },
@@ -37,6 +41,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/FoodRecipe.png',
       date: '2020-2021',
+      link:'https://food-recipe-forkify.netlify.app/',
       desc:
       'A web application that lets users find recipes of food around the world, add recipes that the users like as favorites, and add the ingredients of the food in their cart.'
      
@@ -46,6 +51,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/Hangman.png',
       date: '2020-2021',
+      link:'https://github.com/ManisMalakar/Multi-Client-Server-Hangman',
       desc:
      'A multiplayer Hangman game using Java socket programming in which a Server creates the game in Local Area Network for up to 30 players. '
     },
@@ -54,6 +60,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/StockAnalysis.png',
       date: '2020-2021',
+      link:'',
       desc:
      'Using R-cloud, and common indicators, built a model to  to compare three companies, and predicted profitable investment'
     },
@@ -62,6 +69,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/MoneyHandler.png',
       date: '2020-2021',
+      link:'https://moneyhandler.netlify.app/',
       desc:
      'A Fully manual budget web app to track your expenses.'
     },
@@ -70,6 +78,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/PigGame.png',
       date: '2020-2021',
+      link:'https://piggameforfun.netlify.app/',
       desc:
      'A simple dice game described as a "jeopardy dice game" using JavaScript'
     },
@@ -78,6 +87,7 @@ const data = [
       subtitle: 'Caldwell',
       image: '/images/projects/tictactoe.png',
       date: '2020-2021',
+      link:'https://github.com/ManisMalakar/ClientServerTicTacToe',
       desc:
      'A multiplayer Tic-Tac-Toe game using Java socket programming in which a Server creates the game in Local Area Network. '
     },
